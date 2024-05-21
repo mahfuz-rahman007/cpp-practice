7097f22491b6e2f32801d5b13cf2737ad5e71a10
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-05-21 13:35:38 +0600

    function overloading

--- index.cpp
+++ index.cpp
@@ -2,34 +2,19 @@
 #include <string>
 using namespace std;
 
+int myFunc(int num1, int num2){
+    return num1+num2;
+}
+
+double myFunc(double num1, double num2){
+    return num1+num2;
+}
+
 int main() {
     
-    struct CarModel
-    {
-       int number;
-       string color;
-       string model; 
-    } car1, car2 ;
+    cout << myFunc(2,3) <<endl;
 
-    car1.number = 1;
-    car1.color = "red";
-    car1.model = "BMW";
-    
-    car2.number = 2;
-    car2.color = "green";
-    car2.model = "Audi";
-
-    CarModel car3;
-
-    car3.number = 3;
-    car3.color = "blue";
-    car3.model = "Hyundai";
-
-
-    cout << car1.number << " " << car1.color << " " << car1.model << endl;
-    cout << car2.number << " " << car2.color << " " << car2.model << endl;
-    cout << car3.number << " " << car3.color << " " << car3.model << endl;
-
+    cout << myFunc(2.54,3.98) <<endl;
 
     return 0;
 }
