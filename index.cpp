2ccabfe797203afafaa9405675f8bd2768ee382e
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-05-21 12:49:04 +0600

    struct variable and named datatype options

--- index.cpp
+++ index.cpp
@@ -4,15 +4,31 @@ using namespace std;
 
 int main() {
     
+    struct CarModel
+    {
+       int number;
+       string color;
+       string model; 
+    } car1, car2 ;
 
-    string fullName;
+    car1.number = 1;
+    car1.color = "red";
+    car1.model = "BMW";
+    
+    car2.number = 2;
+    car2.color = "green";
+    car2.model = "Audi";
+
+    CarModel car3;
+
+    car3.number = 3;
+    car3.color = "blue";
+    car3.model = "Hyundai";
 
 
-    cout << "Enter Your Full Name" << endl;
-
-    getline(cin, fullName);
-
-    cout << fullName << endl;
+    cout << car1.number << " " << car1.color << " " << car1.model << endl;
+    cout << car2.number << " " << car2.color << " " << car2.model << endl;
+    cout << car3.number << " " << car3.color << " " << car3.model << endl;
 
 
     return 0;
