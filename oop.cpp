4c1b9c38a129984306045765e2391a02669fcb2e
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-05-21 16:03:59 +0600

    ibheritence concept

--- oop.cpp
+++ oop.cpp
@@ -2,27 +2,26 @@
 #include <string>
 using namespace std;
 
-class Car {
-    public:
-        int year;
-        string brand;
-        string model;
-        Car(int x, string y, string z) {
-            year = x;
-            brand = y;
-            model = z;
-        }
+// Base class
+class Vehicle {
+  public:
+    string brand = "Ford";
+    void honk() {
+      cout << "Tuut, tuut! \n" ;
+    }
+};
+
+// Derived class
+class Car: public Vehicle {
+  public:
+    string model = "Mustang";
 };
 
 int main() {
-
-    Car car1(1, "Audi", "KSJ23");
-
-    Car car2(342, "Sujuki", "Gyt%^");
-
-    cout << car1.year << " " << car1.brand << " " << car1.model <<endl;
-    cout << car2.year << " " << car2.brand << " " << car2.model <<endl;
-    return 0;
+  Car myCar;
+  myCar.honk();
+  cout << myCar.brand + " " + myCar.model;
+  return 0;
 }
 
 
